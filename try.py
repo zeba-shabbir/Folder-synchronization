ba41bbf41d5cbb8b1c930d64d2fe4573c58c65b5
zeba-shabbir <112920843+zeba-shabbir@users.noreply.github.com>
2023-03-13 19:55:11 +0100

    Update try.py

--- try.py
+++ try.py
@@ -3,9 +3,6 @@ from datetime import datetime
 
 def folder_synchronization(srcpath,destinationpath,logfile_path):
      
-#  path = "C:/Users/zebashabbir/Desktop/source"
-#  destinationpath="C:/Users/zebashabbir/Desktop/destination"
-
  logging.basicConfig(filename=logfile_path+"logfile.txt" , level=logging.INFO)
  logging.info(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
  destlist= os.listdir(destinationpath)
@@ -21,7 +18,7 @@ def folder_synchronization(srcpath,destinationpath,logfile_path):
          deletefiles_list.append(i)
          continue    
     
-     shutil.copy("C:/Users/zebashabbir/Desktop/source/"+i ,"C:/Users/zebashabbir/Desktop/destination/")
+     shutil.copy(srcpath+i ,destinationpath)
      logging.info("copying file..."+i)
      print("copying file... "+ i)
 
@@ -35,34 +32,28 @@ def folder_synchronization(srcpath,destinationpath,logfile_path):
      if flag==0:
             print("deleting file from dst folder...",i)
             logging.info("deleting file from destination folder... "+i)
-            os.remove("C:/Users/zebashabbir/Desktop/destination/"+i)
+            os.remove(destinationpath+i)
             
-
-
 
     
  # delete file from destination and then recreate similar to src so that updated one is in dst folder
 
  for i in deletefiles_list:
-     os.remove("C:/Users/zebashabbir/Desktop/destination/"+i)
+     os.remove(destinationpath+i)
      logging.info("deleting file from dst folder.. "+i)
      print("deleting file ..."+i)
 
  for i in deletefiles_list:
-     shutil.copy("C:/Users/zebashabbir/Desktop/source/"+i ,"C:/Users/zebashabbir/Desktop/destination/")
+     shutil.copy(srcpath+i ,destinationpath)
      print("creating file..."+i)
      logging.info("creating file in dst folder.."+i)
 
 ##################################################
 
-def myfunc(count):
- print(datetime.now())
- print(count)
-
 srcpath=input("Please enter source folder path.. ")
 dstpath=input("please enter destination folder path.. ")
 logfile_path=input("Please enter log file path.. ")
-interval=input("PLease enter time in seconds after which you want the synchronization to be performed")
+interval=input("PLease enter time in seconds after which you want the synchronization to be performed.. ")
 
 # example folder path for log file : C:/Users/zebashabbir/Desktop/
 
@@ -70,16 +61,12 @@ interval=float(interval)
 
 next=time.time()
 
-count=1
+
 while True: 
   next =next+interval
-#   myfunc(count)
   folder_synchronization(srcpath,dstpath,logfile_path)
   time.sleep(max(0, next - time.time()))
-  count=count+1
  
-
-
-
+ 
 
     
